7544eb6bb6b57552576db84ad4b6454f9cbe1a32
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2022-03-06 16:20:18 +0700

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use std::io::stdin;
 
 fn main() {
+    println!("Enter some words: ");
     let stdin = stdin();
     let mut sentence = String::new();
     if let Err(_) = stdin.read_line(&mut sentence) {
         println!("Invalid input");
         return;
     }
-    println!("{}", sentence);
+    let vowels = "aiueo";
+    let words = sentence.trim().split_whitespace();
+    let mut pig_latin: Vec<String> = Vec::new();
+    for word in words {
+        if vowels.contains(&word[..1]) {
+            pig_latin.push(format!("{}-hay", &word));
+            continue;
+        }
+        pig_latin.push(format!("{}{}ay", &word[1..], &word[..1]))
+    }
+    println!("{}", pig_latin.join(" "));
 }
